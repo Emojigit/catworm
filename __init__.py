e965a1d8b71b3232f5ffe239a6b03d8939f68cbd
Emoji <55009343+Emojigit@users.noreply.github.com>
2022-05-24 16:35:51 +0800

    貓貓蟲！

---
+++ __init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Emoji"
+__version__ = "1.0.0"
+__url__ = "https://github.com/Emojigit/catworm"
+__description__ = "Catworm by Yan"
+__dname__ = "catworm"
+
+from telethon import events
+cats = [
+    "CAADBQADOgQAAgGegFYOH_O1Sy_hJgI",
+    "CAADBQADcAMAApDLgVac4W-yRc548QI",
+    "CAADBQADMQQAAn0ZgFY0TkXJmOwNlwI"
+]
+def setup(bot):
+    @bot.on(events.NewMessage(pattern='/catworm'))
+    async def catworm(event):
+        async with bot.action(event.chat, 'sticker') as action:
+            for x in cats:
+                await bot.send_file(event.chat,file=x,silent=True)
+        raise events.StopPropagation
